4d299feb90296a401a4988557b5be59817665bf8
kurtreyn <kurtreyn@yahoo.com>
2022-02-25 16:56:39 -0500

    passing data between screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,52 @@
-import { SafeAreaView, Text, View, Button } from 'react-native';
-import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
 import { globalStyles } from '../styles/global';
 
 const Home = ({ navigation }) => {
-  const pressHandler = () => {
-    navigation.navigate('ReviewDetails');
-    // COULD ALSO USE:
-    // navigation.push('ReviewDetails');
-  };
+  // const pressHandler = () => {
+  //   navigation.navigate('ReviewDetails');
+  //   // COULD ALSO USE:
+  //   // navigation.push('ReviewDetails');
+  // };
+
+  const [reviews, setReviews] = useState([
+    {
+      title: 'Zelda, Breath of Fresh Air',
+      rating: 5,
+      body: 'lorem ipsum',
+      key: 1,
+    },
+    {
+      title: 'Gotta Catch Them All (again)',
+      rating: 4,
+      body: 'lorem ipsum',
+      key: 2,
+    },
+    { title: 'Not So Final Fantasy', rating: 3, body: 'lorem ipsum', key: 3 },
+  ]);
 
   return (
     <SafeAreaView style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home screen</Text>
-      <Button title="GO TO REVIEW DETAILS" onPress={pressHandler} />
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            // PASSING DATA BETWEEN SCREENS - STACK NAVIGATION
+            onPress={() => navigation.navigate('ReviewDetails', item)}
+          >
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* <Text style={globalStyles.titleText}>Home screen</Text> */}
+      {/* <Button title="GO TO REVIEW DETAILS" onPress={pressHandler} /> */}
     </SafeAreaView>
   );
 };
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -2,15 +2,22 @@ import { Button, Text, View } from 'react-native';
 import React from 'react';
 import { globalStyles } from '../styles/global';
 
-const ReviewDetails = ({ navigation }) => {
-  const pressHandler = () => {
-    navigation.goBack();
-  };
+const ReviewDetails = ({ route }) => {
+  // const pressHandler = () => {
+  //   navigation.goBack();
+  // };
+
+  // PASSING DATA BETWEEN SCREENS - STACK NAVIGATION
+  const { title } = route.params;
+  const { body } = route.params;
+  const { rating } = route.params;
 
   return (
     <View style={globalStyles.container}>
-      <Text>Review Details</Text>
-      <Button title="BACK TO HOME SCREEN" onPress={pressHandler} />
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+      <Text>{rating}</Text>
+      {/* <Button title="BACK TO HOME SCREEN" onPress={pressHandler} /> */}
     </View>
   );
 };
