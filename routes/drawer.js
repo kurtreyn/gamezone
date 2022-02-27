8be5e6c8f90049019ae94c3bbd5cbcf78196a158
kurtreyn <kurtreyn@yahoo.com>
2022-02-27 16:41:10 -0500

    background images

--- routes/drawer.js
+++ routes/drawer.js
@@ -1,4 +1,5 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Image, Text, View, StyleSheet, ImageBackground } from 'react-native';
 import HomeStack from './homeStack';
 import AboutStack from './aboutStack';
 
@@ -17,12 +18,49 @@ const RootDrawerNavigator = () => {
       <Drawer.Screen
         name="Home"
         component={HomeStack}
-        options={{ headerShown: false }}
-        options={{ title: 'GameZone' }}
+        options={{
+          headerTitle: () => (
+            <ImageBackground
+              source={require('../assets/game_bg.png')}
+              style={styles.header}
+            >
+              <View style={styles.headerTitle}>
+                <Image
+                  style={styles.headerImage}
+                  source={require('../assets/heart_logo.png')}
+                  headerTitle="GameZone"
+                />
+                <Text style={styles.headerText}>GameZone</Text>
+              </View>
+            </ImageBackground>
+          ),
+        }}
       />
       <Drawer.Screen name="About" component={AboutStack} />
     </Drawer.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    flexDirection: 'row',
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
 
 export default RootDrawerNavigator;
